432d692030a6c8ff584c89817d91c2152f37f29b
shaliniyadav <shaliniyadav.ait@gmail.com>
2025-06-26 17:20:04 +0530

    Test

---
+++ Polymorphism/src/ShapeCalculator.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class ShapeCalculator {
+    public static void main(String[] args) {
+        System.out.println("Hello and welcome!");
+        Circle c = new Circle(3);
+        Triangle t = new Triangle();
+        Rectangle r = new Rectangle();
+
+
+    };
+}
