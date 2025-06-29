80a246454155ae6d169d42d3a99f17398536591f
shaliniyadav <shaliniyadav.ait@gmail.com>
2025-06-29 22:11:40 +0530

    Refactored the code

--- Polymorphism/src/ShapeCalculator.java
+++ Polymorphism/src/ShapeCalculator.java
@@ -5,6 +5,30 @@ import java.util.ArrayList;
 public class ShapeCalculator {
     public static void main(String[] args) {
         System.out.println("Calculating the shape : ");
+        ArrayList<Shape> shapes = shapeList();
+        displayShapes(shapes);
+        rectangleToSquare();
+    }
+
+    private static void rectangleToSquare() {
+        Rectangle s = new Rectangle(3,4);
+        s.setDimensions(4);
+        System.out.println("Shape is : "+  s.shape());
+        System.out.println("Area is : " + s.calculateArea());
+        System.out.println("Perimeter is : " + s.calculatePerimeter());
+        System.out.println(" **************************");
+    }
+
+    private static void displayShapes(ArrayList<Shape> shapes) {
+        for (Shape type: shapes){
+            System.out.println("Shape is : "+  type.shape());
+            System.out.println("Area is : " + type.calculateArea());
+            System.out.println("Perimeter is : " + type.calculatePerimeter());
+            System.out.println(" **************************");
+        }
+    }
+
+    private static ArrayList<Shape> shapeList() {
         Shape r = new Rectangle(3,4);
         Shape c = new Circle(3);
         Shape t = new Triangle(3,3,3);
@@ -12,18 +36,6 @@ public class ShapeCalculator {
         shapes.add(r);
         shapes.add(c);
         shapes.add(t);
-        for (Shape type: shapes){
-            System.out.println("Shape is : "+  type.shape());
-            System.out.println("Area is : " + type.calculateArea());
-            System.out.println("Perimeter is : " + type.calculatePerimeter());
-            System.out.println(" **************************");
-        }
-
-        Rectangle s = new Rectangle(3,3);
-        s.setDimensions(4);
-        System.out.println("Shape is : "+  s.shape());
-        System.out.println("Area is : " + s.calculateArea());
-        System.out.println("Perimeter is : " + s.calculatePerimeter());
-        System.out.println(" **************************");
+        return shapes;
     }
 }
