aee8d70b4cd63fdbf37ac3d1bc4ea64c8a4c2bd5
shaliniyadav <shaliniyadav.ait@gmail.com>
2025-06-30 14:49:10 +0530

    Added another loop

--- Practice/30th June/src/MultiThreading.java
+++ Practice/30th June/src/MultiThreading.java
@@ -2,19 +2,15 @@ public class MultiThreading {
   public static void main(String[] args){
       MyThread firstThread = new MyThread();
       MyThread secondThread = new MyThread();
-
       firstThread.start();
       secondThread.start();
-      // Normal method execution
-//      firstThread.run();
-//      secondThread.run();
-//      firstThread.show();
-//      secondThread.show();
+      for (int dis = 0; dis < 50; dis++) {
+          System.out.println("In Main. " + dis);
+      }
   }
 }
 
 class MyThread extends Thread{
-//    void show(){
     public void run(){
         for (int distance = 0; distance < 50; distance++) {
             System.out.println("Here! " + distance);
