8b36176ac55dcc3017e06ac31a50a29e7234a596
shaliniyadav <shaliniyadav.ait@gmail.com>
2025-06-30 15:38:46 +0530

    Understanding Runnable

---
+++ Practice/30th June/src/MyThreadRunnable.java
@@ -0,0 +1,15 @@
+class MyThreadRunnable implements Runnable {
+ public void run(){
+     for (int km = 0; km < 50; km++){
+         System.out.println(Thread.currentThread().getName());
+     }
+ }
+}
+
+class MainClassRunnable{
+    public static void main(String[] args){
+        MyThreadRunnable th1 = new MyThreadRunnable();
+        Thread t1 = new Thread(th1);
+        t1.start();
+    }
+}
