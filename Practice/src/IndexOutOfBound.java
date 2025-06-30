3f6af695f485e9ad9eb9f744d745bb31402dd6a7
shaliniyadav <shaliniyadav.ait@gmail.com>
2025-06-30 12:15:25 +0530

    Index out of bound exception

--- Practice/src/IndexOutOfBound.java
+++ Practice/src/IndexOutOfBound.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
-
 public class IndexOutOfBound {
+    public static void main(String[] args) {
+        System.out.println("Start");
+        try {
+            int nums[] = {10, 20, 30};
+            System.out.println(nums[4]);
+        }
+
+        catch (IndexOutOfBoundsException e) {
+            System.out.println("Index out of bound!");
+        }
+
+        catch (Exception e){
+            System.out.println("Exception");
+        }
+
+//        catch (IndexOutOfBoundsException e) {
+//            System.out.println("Index out of bound!");
+//        }
+
+        System.out.println("End");
+    }
+
 }
