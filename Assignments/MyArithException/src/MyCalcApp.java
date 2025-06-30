83ed05f7956051a20fc0c3b4707068d31fe365ce
shaliniyadav <shaliniyadav.ait@gmail.com>
2025-06-30 17:22:29 +0530

    Refactored Exception Assignment

--- Assignments/MyArithException/src/MyCalcApp.java
+++ Assignments/MyArithException/src/MyCalcApp.java
@@ -1,22 +1,3 @@
-class MyArithException extends RuntimeException{
-    public  MyArithException(String message){
-        super(message);
-    }
-}
-
-class Calculator {
-    double caldouble(int n) throws MyArithException{
-            if (n < 0) {
-                throw new MyArithException("Age cannot be negative.");
-            } else if (n == 0) {
-                throw new MyArithException("Age cannot be zero.");
-            }
-        double newN = (double) n;
-        System.out.println("New value of n is " + newN);
-        return (newN);
-    }
-}
-
 public class MyCalcApp {
     public static void main(String[] args) {
         Calculator c = new Calculator();
