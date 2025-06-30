40880d595d9678245191919d78a11f0499aaea30
shaliniyadav <shaliniyadav.ait@gmail.com>
2025-06-30 15:16:35 +0530

    Added thread name

--- Practice/30th June/src/MultiThreading.java
+++ Practice/30th June/src/MultiThreading.java
@@ -3,7 +3,9 @@ public class MultiThreading {
       MyThread firstThread = new MyThread();
       MyThread secondThread = new MyThread();
       firstThread.start();
+      firstThread.setName("First");
       secondThread.start();
+      secondThread.setName("Second");
       for (int dis = 0; dis < 50; dis++) {
           System.out.println("In Main. " + dis);
       }
@@ -13,7 +15,7 @@ public class MultiThreading {
 class MyThread extends Thread{
     public void run(){
         for (int distance = 0; distance < 50; distance++) {
-            System.out.println("Here! " + distance);
+            System.out.println("Here! " + distance + " " +Thread.currentThread().getName());
         }
     }
 }
