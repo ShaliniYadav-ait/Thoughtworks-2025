37089a7f419575164b284e17588ed4c89d6c7aad
shaliniyadav <shaliniyadav.ait@gmail.com>
2025-07-01 14:30:27 +0530

    capacity

---
+++ Practice/1st July/src/Guide_emp.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Vector;
+
+public class Guide_emp {
+    public static void main(String[] args){
+        Vector<String> listOfNames = new Vector<>();
+
+        listOfNames.add("Julie");
+        listOfNames.add("David");
+        listOfNames.add("John");
+        listOfNames.add("Jane");
+        listOfNames.add("Julie");
+        listOfNames.add("David");
+        listOfNames.add("John");
+        listOfNames.add("Jane");
+
+        System.out.println(listOfNames.size());
+        System.out.println(listOfNames);
+        System.out.println(listOfNames.capacity());
+        listOfNames.trimToSize();
+        System.out.println(listOfNames.capacity());
+    }
+}
